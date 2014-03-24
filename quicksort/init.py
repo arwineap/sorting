98c3b2bc25b5fba7c744fa0b54a92a2dcaa13291
alex <arwineap@gmail.com>
2014-03-23 22:50:36 -0700

    Adding 'sorted' quicksort solution

--- quicksort/init.py
+++ quicksort/init.py
@@ -5,51 +5,40 @@ import random
 
 
 # Goal: Implement sorting function using concepts learned from quicksort
-def quicksort(n):
-    origsort = pivotlist(n)
-    newsort = pivotlist(origsort)
-    gosort = newsort
-    go = True
-    i = 0
-    while go:
-        go1sort = pivotlist(gosort)
-        go2sort = pivotlist(go1sort)
-        go3sort = pivotlist(go2sort)
-        if gosort == go1sort and go1sort == go2sort and go3sort == go2sort:
-            print 'winner?'
-            go = False
-        else:
-            print 'gosort', gosort
-            print 'go1sort', go1sort
-            print 'go2sort', go2sort
-            print 'go3sort', go3sort
-            gosort = go3sort
-        i += 1
-    return (gosort, i)
-
-def pivotlist(alist):
-    #pivot = alist.pop()
-    element = random.randrange(0,len(alist)-1)
+def pivotalist(alist):
+    element = random.randrange(0, len(alist)-1)
     pivot = alist[element]
-    alist.remove(alist[element])
     greater = []
     lesser = []
+    pivotList = []
     for entry in alist:
         if entry > pivot:
             greater.append(entry)
-        elif entry <= pivot:
+        elif entry < pivot:
             lesser.append(entry)
-    #print lesser, pivot, greater
-    return lesser + [pivot] + greater
+        else:
+            pivotList.append(entry)
+    resultList = lesser + pivotList + greater
+    return resultList
 
+#randomlist = [5, 6, 3, 2, 1, 4, 7, 10, 9, 8]
+#randomlist = [8, 6, 6, 1, 6, 5, 6, 8, 2]
 
-#randomlist = []
-#for x in range(1, 10):
-#    randomlist.append(random.randrange(1,10))
+randomlist = []
+for x in range(1, 10000):
+    randomlist.append(random.randrange(1, 10000))
 
-# disable random list gen, use this one:
-randomlist = [8, 6, 6, 1, 6, 5, 6, 8, 2]
+matchX = 0
+count = 0
+a = pivotalist(randomlist)
+while matchX != 10:
+    c = a
+    b = pivotalist(a)
+    if b == a:
+        matchX += 1
+    else:
+        a = b
+    count += 1
 
-print randomlist
-randomlist = quicksort(randomlist)
-print 'quicksort in %d iterations:' % randomlist[1], randomlist[0]
+print "it took %d iterations to sort this list of %d entries" % (count, len(randomlist))
+#print a
